9dfb560bb2c0bf92398023708c55ef0e8d3bedd0
Alan Nix <alan.nix@lacework.net>
2022-06-15 16:10:32 -0400

    fix: resolved priority class name merge collision

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -11,12 +11,12 @@ readonly project_name=terraform-kubernetes-agent
 TEST_CASES=(
   examples/custom-agent-configuration
   examples/custom-interface-connection-size
+  examples/custom-priority-class-name
   examples/custom-proxy-url
   examples/custom-resource-allocation
   examples/custom-server-url
   examples/custom-tolerations
   examples/custom-upgrade-behavior
-  examples/priority-class-name
   examples/default
 )
 
