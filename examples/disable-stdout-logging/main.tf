bb807d0f7ed19be9384fd92a398e01d061d9a4a6
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2023-04-21 11:30:23 +0100

    feat: add new variable 'lacework_agent_log_stdout' (#65)
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

---
+++ examples/disable-stdout-logging/main.tf
@@ -0,0 +1,15 @@
+provider "kubernetes" {}
+
+provider "lacework" {}
+
+resource "lacework_agent_access_token" "k8s" {
+  name        = "prod"
+  description = "k8s deployment for production env"
+}
+
+module "lacework_k8s_datacollector" {
+  source = "../../"
+
+  lacework_access_token    = lacework_agent_access_token.k8s.token
+  lacework_agent_log_stdout = false
+}
